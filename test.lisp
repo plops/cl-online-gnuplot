a42566e3179c84d2338dd796d4ca53ecf372cb76
Martin Kielhorn <kielhorn.martin@gmail.com>
2015-06-03 07:39:12 +0200

    call fftw and bessel function in the test. i'm trying to draw a circle

--- test.lisp
+++ test.lisp
@@ -12,6 +12,40 @@
     (plot l)
     (sleep .1)))
 
+(cffi:defcfun j1 :double (x :double))
+
+
+(let* ((w 37)
+       (h 12)
+       ;; allocate a 1d array
+       (a1 (make-array (* w h) :element-type '(complex double-float)))
+       ;; create a 2d array for access
+       (a (make-array (list h w) :element-type '(complex double-float)
+   ;                   :displaced-to a1
+		      )))
+
+  ;; fill the 2d array with a sinosoidal grating
+  (dotimes (i w)
+    (dotimes (j h)
+      (setf (aref a j i) (complex (let ((r (sqrt (+ (expt (- (/ i w) .5d0) 2)
+						    (expt (- (/ j h) .5) 2)))))
+				    (if (< (abs r) 1d-9)
+					1d0
+					(/ (j1 r) r)))))))
+
+  ;; call fftw
+  (defparameter *bla* (fftw:ft a))
+
+  ;; print out each element of the array. scale data to lie within 0..9
+  (progn
+    (terpri)
+    (destructuring-bind (h w) (array-dimensions *bla*)
+      (dotimes (j h)
+        (dotimes (i w)
+          (format t "~1,'0d" (floor (abs (aref *bla* j i)) (/ (* h w) 9))))
+        (terpri)))))
+
+
 (loop for b from .1 upto 2.3 by .1 do
      (cl-online-gnuplot::gnuplot-send (format nil "
 set zrange [0:500]
